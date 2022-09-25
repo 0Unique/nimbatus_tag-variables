0ea0818d19e73320cadb3043efc0c9f77c485f34
UniqueName <75515676+UniqueName0@users.noreply.github.com>
2022-09-25 15:14:31 -0400

    yes

---
+++ Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using Assets.Nimbatus.Scripts.WorldObjects.Items.DroneParts;
+using Assets.Nimbatus.Scripts.WorldObjects.Items.DroneParts.SensorParts;
+using Assets.Nimbatus.Scripts.WorldObjects.Items.Selection;
+using BepInEx;
+using BepInEx.Preloader;
+using HarmonyLib;
+using NCalc;
+using Sirenix.Utilities;
+using UnityEngine;
+
+namespace Nimbatus_tag_variables
+{
+
+    [BepInProcess("Nimbatus.exe")]
+    [BepInPlugin("uniquename.nimbatus.tag-vars", "tag-vars", "0.0.0.0")]
+    public class Nimbatus_tag_variables : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            var harmony = new Harmony("uniquename.nimbatus.tag-vars");
+            harmony.PatchAll();
+        }
+    }
+    
+    
+    [HarmonyPatch(typeof(EventKeyHub), "PressKey", argumentTypes: new Type[] {typeof(bool), typeof(string)})]
+    public class PressKey_Patch
+    {
+        private static Dictionary<string, int> vars = new Dictionary<string, int>{};
+        private static Dictionary<string, string> active = new Dictionary<string, string>{};
+
+        public static void Prefix(ref bool press, ref string keyCode, EventKeyHub __instance)
+        {
+            string orig_keyCode = keyCode;
+            
+            Regex rx = new Regex(@"\{([^\}]+)\}"); //regex to find things within {}
+            MatchCollection equations = rx.Matches(keyCode);
+            
+            for (int count = 0; count < equations.Count; count++)
+            {
+                string[] tmp2 = equations[count].Value.Replace("{", "").Replace("}", "").Split('=');
+
+                foreach (KeyValuePair<string, int> var in vars)
+                {
+                    tmp2[tmp2.Length - 1] = tmp2[tmp2.Length - 1].Replace(var.Key, var.Value.ToString());
+                }
+                
+
+                Expression e = new Expression(tmp2[tmp2.Length - 1]);
+                var ans = e.Evaluate();
+
+                if (tmp2.Length > 1 && !press)
+                {
+                    vars[tmp2[0].Replace(" ", "")] = int.Parse(ans.ToString());
+                    
+                }
+                
+                keyCode = keyCode.Replace(equations[count].ToString(), int.Parse(ans.ToString()).ToString());
+            }
+            
+            
+            if (press)
+            {
+                active.Add(orig_keyCode, keyCode);
+            }   
+            else if (active.ContainsKey(orig_keyCode))
+            {
+                keyCode = active[orig_keyCode];
+                active.Remove(orig_keyCode);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nimbatus_tag-variables
  
+allows you to put variables into tags which can be changed and access mid game
+
+variables are put within {}
+to change a value you put an = after what you want to change, like this
+{varA = 1+1}
+you can also use the var in the assignment, like this
+{varA = varA + 1}
+
+can do basic math: addition (+), subtraction (-), multiplication (\*), division (\\)
