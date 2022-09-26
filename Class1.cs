e825a61dfd402d2f529ea7ebd2425732a5b266a0
UniqueName <75515676+UniqueName0@users.noreply.github.com>
2022-09-26 14:09:24 -0400

    literally just added comments

--- Class1.cs
+++ Class1.cs
@@ -38,43 +38,51 @@ namespace Nimbatus_tag_variables
 
         public static void Prefix(ref bool press, ref string keyCode, EventKeyHub __instance)
         {
+            //keyCode is the tag
             string orig_keyCode = keyCode;
             
-            Regex rx = new Regex(@"\{([^\}]+)\}"); //regex to find things within {}
+            //regex to find things within {}
+            Regex rx = new Regex(@"\{([^\}]+)\}"); 
             MatchCollection equations = rx.Matches(keyCode);
             
+            //iterates through each substring within {} found
             for (int count = 0; count < equations.Count; count++)
             {
                 string[] tmp2 = equations[count].Value.Replace("{", "").Replace("}", "").Split('=');
 
+                //replaces var names with their value within tag
                 foreach (KeyValuePair<string, int> var in vars)
                 {
                     tmp2[tmp2.Length - 1] = tmp2[tmp2.Length - 1].Replace(var.Key, var.Value.ToString());
                 }
                 
-
+                // gets value of equation
                 Expression e = new Expression(tmp2[tmp2.Length - 1]);
                 var ans = e.Evaluate();
 
-                if (tmp2.Length > 1 && !press)
-                {
-                    vars[tmp2[0].Replace(" ", "")] = int.Parse(ans.ToString());
-                    
-                }
                 
+                if (tmp2.Length > 1 && !press) //!press = release of key
+                {
+                    vars[tmp2[0].Replace(" ", "")] = int.Parse(ans.ToString()); //right of = is assigned to left var
+                    // removes spaces in case it is written like {a = 5 + 5} or { a = 5 + 5 }, so that it will just be "a" and not "a " or " a "
+                }
+
+                // replaces equation with it's value
                 keyCode = keyCode.Replace(equations[count].ToString(), int.Parse(ans.ToString()).ToString());
             }
             
             
+            //added to fix problem with releasing keys
             if (press)
             {
                 active.Add(orig_keyCode, keyCode);
-            }   
+            }
             else if (active.ContainsKey(orig_keyCode))
             {
                 keyCode = active[orig_keyCode];
                 active.Remove(orig_keyCode);
             }
+            
         }
     }
 }
